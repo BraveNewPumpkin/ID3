5ad3f31100c75001815bb2b5a5b8582d332689c2
kdbolton <latestthing@gmail.com>
2017-09-19 13:51:54 -0500

    added command line argument functionality

--- TreeMaker.py
+++ TreeMaker.py
@@ -1,10 +1,27 @@
 import pandas as pd
+import sys
 from pathlib import Path
 from treelib import Node, Tree
 
-input_dirpath = Path('D:','Users','Kyle','Downloads','school','machine_learning','data_sets1')
+usage = 'TreeMaker.py /path/to/training/dataset /path/to/validation/dataset /path/to/testing/dataset pruning_factor'
 
-training_set = pd.read_csv(Path(input_dirpath, 'training_set.csv'))
+if len(sys.argv) < 5:
+    print('not enough arguments\n')
+    print(usage)
+    sys.exit(1)
+if len(sys.argv) > 5:
+    print('too many arguments\n')
+    print(usage)
+    sys.exit(1)
+
+training_data_path = Path(sys.argv[1])
+validation_data_path = Path(sys.argv[2])
+test_data_path = Path(sys.argv[3])
+pruning_factor = sys.argv[4]
+
+training_set = pd.read_csv(training_data_path)
+validation_set = pd.read_csv(validation_data_path)
+test_set = pd.read_csv(test_data_path)
 
 print(training_set)
 
