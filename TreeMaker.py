292e092de8f4e1523502f649bdea10061694ede7
kdbolton <latestthing@gmail.com>
2017-09-19 16:21:57 -0500

    calculate initial entropy

--- TreeMaker.py
+++ TreeMaker.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sys
 from pathlib import Path
 from treelib import Node, Tree
+from math import log2
 
 usage = 'TreeMaker.py /path/to/training/dataset /path/to/validation/dataset /path/to/testing/dataset pruning_factor'
 
@@ -26,6 +27,24 @@ test_set = pd.read_csv(test_data_path)
 header = list(training_set.columns.values)
 print(header)
 
+num_tuples = len(training_set[header[-1]])
+num_class = {}
+
+
+for val in training_set[header[-1]]:
+    if val in num_class:
+        num_class[val] += 1
+    else:
+        num_class[val] = 1
+
+probability_class = {}
+for key in num_class:
+    probability_class[key] = num_class[key] / num_tuples
+
+previous_entropy = 0
+for key in probability_class:
+    previous_entropy += -1 * probability_class[key] * log2(probability_class[key])
+print(previous_entropy)
 
 # print(training_set)
 
