62217c7efeca04879fbe556f352126602b0f2f66
kdbolton <latestthing@gmail.com>
2017-09-24 22:29:07 -0500

    made output more clear

--- Main.py
+++ Main.py
@@ -54,9 +54,9 @@ def main(argv):
     validation_set_accuracy = calcAccuracyForData(validation_set, headers, tree)
     test_set_accuracy = calcAccuracyForData(test_set, headers, tree)
 
-    print('Accuracy of the model on the training dataset = %.2f%%' % training_set_accuracy)
-    print('Accuracy of the model on the validation dataset = %.2f%%' % validation_set_accuracy)
-    print('Accuracy of the model on the test dataset = %.2f%%' % test_set_accuracy)
+    print('Accuracy of the model on the training dataset after pruning = %.2f%%' % training_set_accuracy)
+    print('Accuracy of the model on the validation dataset after pruning = %.2f%%' % validation_set_accuracy)
+    print('Accuracy of the model on the test dataset after pruning = %.2f%%' % test_set_accuracy)
 
     return 0
 
